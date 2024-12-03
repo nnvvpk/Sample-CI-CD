328a74ecc35f6f986070ea23fe7c92aaba771681
nnvvpk <praveenkumar.n@istpl.co>
2024-12-03 16:31:27 +0530

    REmoved dotenv

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-require("dotenv").config()
+// require("dotenv").config()
 const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
-
+console.log("The port is:", process.env.PORT);
 app.listen(process.env.PORT);
 
 // app.listen(3000)
